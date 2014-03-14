f00cb537d77cdcedd3640638e444e875ab9b0875
Ada Rose Edwards <ada.edwards@ft.com>
2014-03-14 13:16:01 +0000

    Tweaked gitignore to ignore commonjs built file

--- app/commonjs/index.js
+++ app/commonjs/index.js
@@ -3,20 +3,26 @@ var $ = function (str) {
 	return document.querySelector(str);
 };
 
-$('#meButton').addEventListener('click', function () {
-	if (!$('.row').classList.contains('left')) {
-		$('.row').classList.add('left');
-		$('.row').classList.remove('right');
-	} else {
-		$('.row').classList.remove('left');
-	}
-});
-
-$('#commentButton').addEventListener('click', function () {
+function openLeft(e) {
+	e.preventDefault();
 	if (!$('.row').classList.contains('right')) {
 		$('.row').classList.add('right');
 		$('.row').classList.remove('left');
 	} else {
 		$('.row').classList.remove('right');
 	}
-});
+}
+
+function openRight(e) {
+	e.preventDefault(e);
+	if (!$('.row').classList.contains('left')) {
+		$('.row').classList.add('left');
+		$('.row').classList.remove('right');
+	} else {
+		$('.row').classList.remove('left');
+	}
+}
+
+$('#meButton').addEventListener('click', openLeft);
+
+$('#commentButton').addEventListener('click', openRight);

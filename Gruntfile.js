92205ea90d3e64b1e0b503b7b74ba2ca897df22c
Ada Rose Edwards <adaroseedwards@gmail.com>
2014-06-08 11:40:18 +0100

    Updated gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -270,7 +270,7 @@ module.exports = function (grunt) {
 						'!**/_*{,/**}'
 						// Explicitly add any files your site needs for distribution here.
 						//'_bower_components/jquery/jquery.js',
-						//'favicon.ico',
+						'favicon.ico',
 						//'apple-touch*.png'
 					],
 					dest: '<%= yeoman.dist %>'

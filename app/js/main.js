439bf68a5cf3e0d94c91f7d24a5c48b2ccca7bd3
Ada Rose Edwards <ada@ada.is>
2015-01-14 23:16:38 +0000

    Clean videos

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,5 @@
-function activateVideo(item) {
-	if (!item.src && item.dataset.src) {
-		item.src = item.dataset.src;
-	}
-}
-
 Array.prototype.slice.call(document.body.getElementsByTagName('video')).forEach(function (item) {
-	item.addEventListener('click', function (e) {
-		activateVideo(e.target);
-	});
+	video.preload = 'none';
+	video.autoplay = false;
+	video.src = video.dataset.src;
 });

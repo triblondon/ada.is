53d44e0237234021e1b3569cc2bc5a7778d5ca53
Ada Rose Edwards <ada@ada.is>
2015-01-14 23:22:15 +0000

    jshint

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-Array.prototype.slice.call(document.body.getElementsByTagName('video')).forEach(function (item) {
+Array.prototype.slice.call(document.body.getElementsByTagName('video')).forEach(function (video) {
 	video.preload = 'none';
 	video.autoplay = false;
 	video.src = video.dataset.src;
